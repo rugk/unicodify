8bd06657b3509b8f6102aaa7bc95bc1b4bcff24f
rugk <rugk+git@posteo.de>
2021-04-18 20:56:17 +0200

    feat: add RandomTips

--- src/options/options.js
+++ src/options/options.js
@@ -6,6 +6,7 @@
 
 import * as AddonSettings from "/common/modules/AddonSettings/AddonSettings.js";
 import * as AutomaticSettings from "/common/modules/AutomaticSettings/AutomaticSettings.js";
+import * as RandomTips from "/common/modules/RandomTips/RandomTips.js";
 
 import * as CustomOptionTriggers from "./modules/CustomOptionTriggers.js";
 
@@ -13,3 +14,8 @@ import * as CustomOptionTriggers from "./modules/CustomOptionTriggers.js";
 CustomOptionTriggers.registerTrigger();
 AutomaticSettings.setDefaultOptionProvider(AddonSettings.getDefaultValue);
 AutomaticSettings.init();
+
+RandomTips.init("/common/modules/data/Tips.js").then(() => {
+    RandomTips.setContext("options");
+    RandomTips.showRandomTipIfWanted();
+});
