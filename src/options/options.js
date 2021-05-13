8fe1a92d1f343a4228376c0159dfd697f618f05a
rugk <rugk+git@posteo.de>
2021-05-13 15:34:11 +0200

    fix: correctly init random tips

--- src/options/options.js
+++ src/options/options.js
@@ -4,6 +4,7 @@
  * @requires modules/OptionHandler
  */
 
+import { tips } from "/common/modules/data/Tips.js";
 import * as AddonSettings from "/common/modules/AddonSettings/AddonSettings.js";
 import * as AutomaticSettings from "/common/modules/AutomaticSettings/AutomaticSettings.js";
 import * as RandomTips from "/common/modules/RandomTips/RandomTips.js";
@@ -15,7 +16,7 @@ CustomOptionTriggers.registerTrigger();
 AutomaticSettings.setDefaultOptionProvider(AddonSettings.getDefaultValue);
 AutomaticSettings.init();
 
-RandomTips.init("/common/modules/data/Tips.js").then(() => {
+RandomTips.init(tips).then(() => {
     RandomTips.setContext("options");
     RandomTips.showRandomTipIfWanted();
 });
