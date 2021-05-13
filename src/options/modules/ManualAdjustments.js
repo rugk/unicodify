a384beccf9ea595f501d62f9e8e1972989d3b6be
rugk <rugk+git@posteo.de>
2021-05-13 16:29:10 +0200

    refactor: use own module for adjustments

---
+++ src/options/modules/ManualAdjustments.js
@@ -0,0 +1,18 @@
+import { getBrowserValue } from "/common/modules/BrowserCompat.js";
+
+/**
+ * Initializes module.
+ *
+ * Applies the adjustments.
+ *
+ * @returns {Promise}
+ */
+export function init() {
+    getBrowserValue({
+        firefox: "https://addons.mozilla.org/firefox/addon/awesome-emoji-picker/?utm_source=unicodify-addon&utm_medium=addon&utm_content=unicodify-addon-settings-inline&utm_campaign=unicodify-addon-settings-inline",
+        thunderbird: "https://addons.thunderbird.net/thunderbird/addon/awesome-emoji-picker/reviews/?utm_source=unicodify-addon&utm_medium=addon&utm_content=unicodify-addon-settings-inline&utm_campaign=unicodify-addon-settings-inline",
+        chrome: "https://chrome.google.com/webstore/detail/awesome-emoji-picker/",
+    }).then((browserUrl) => {
+        document.getElementById("link-awesome-emoji").href = browserUrl;
+    });
+}
--- src/options/options.js
+++ src/options/options.js
@@ -5,27 +5,20 @@
  */
 
 import { tips } from "/common/modules/data/Tips.js";
-import { getBrowserValue } from "/common/modules/BrowserCompat.js";
 import * as AddonSettings from "/common/modules/AddonSettings/AddonSettings.js";
 import * as AutomaticSettings from "/common/modules/AutomaticSettings/AutomaticSettings.js";
 import * as RandomTips from "/common/modules/RandomTips/RandomTips.js";
 
 import * as CustomOptionTriggers from "./modules/CustomOptionTriggers.js";
+import * as ManualAdjustments from "./modules/ManualAdjustments.js";
 
 // init modules
 CustomOptionTriggers.registerTrigger();
 AutomaticSettings.setDefaultOptionProvider(AddonSettings.getDefaultValue);
 AutomaticSettings.init();
+ManualAdjustments.init();
 
 RandomTips.init(tips).then(() => {
     RandomTips.setContext("options");
     RandomTips.showRandomTipIfWanted();
-});
-
-getBrowserValue({
-    firefox: "https://addons.mozilla.org/firefox/addon/awesome-emoji-picker/?utm_source=unicodify-addon&utm_medium=addon&utm_content=unicodify-addon-settings-inline&utm_campaign=unicodify-addon-settings-inline",
-    thunderbird: "https://addons.thunderbird.net/thunderbird/addon/awesome-emoji-picker/reviews/?utm_source=unicodify-addon&utm_medium=addon&utm_content=unicodify-addon-settings-inline&utm_campaign=unicodify-addon-settings-inline",
-    chrome: "https://chrome.google.com/webstore/detail/awesome-emoji-picker/",
-}).then((browserUrl) => {
-    document.getElementById("link-awesome-emoji").href = browserUrl;
 });
